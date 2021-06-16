ba6b34c4f958b91b1cb55349d5e907fcc78f271c
Fernando <fernanddolucas@gmail.com>
2021-06-16 08:58:57 -0300

    Initial Commit

---
+++ NanoML/NanoMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanoMLApp.swift
+//  NanoML
+//
+//  Created by Fernando de Lucas da Silva Gomes on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct NanoMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
